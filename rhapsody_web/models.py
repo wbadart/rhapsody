9b2723329dc52620583de16beac9ca4d86931dcf
Madeline Kusters <mkusters@nd.edu>
2018-03-20 18:05:58 -0400

    adding stubs for all entity sets

--- rhapsody_web/models.py
+++ rhapsody_web/models.py
@@ -4,8 +4,31 @@ from django.db import models
 class Artist(models.Model):
     pass
 
+class Genre(models.Model):
+    pass
+
+class Album(models.Model):
+    pass
 
 class Song(models.Model):
     artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
     title = models.CharField(max_length=30)
     spotify_id = models.CharField(max_length=22)
+
+class Playlist(models.Model):
+    pass
+
+class RadioStation(models.Model):
+    pass
+
+class Concert(models.Model):
+    pass
+
+class User(models.Model):
+    username = models.CharField(max_length=30, primary_key=True)
+
+class Admin(User):
+    pass
+
+class Regular(User):
+    pass
