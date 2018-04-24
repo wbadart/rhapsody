04acd8c91b0ee8996a13be572d9e2caa62c9297b
amunch <amunch@nd.edu>
2018-04-24 14:11:45 -0400

    Signup page

--- rhapsody_web/views.py
+++ rhapsody_web/views.py
@@ -1,11 +1,27 @@
 import rhapsody_web.models as models
 
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 
 from random import sample
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('home')
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration/signup.html', {'form': form})
 
 @login_required
 def index(req):
