e785de6d5a52c22a92ea7777646f82a68645de3a
amunch <amunch@nd.edu>
2018-04-30 18:20:59 -0400

    Add recommend endpoint

--- rhapsody_web/views.py
+++ rhapsody_web/views.py
@@ -9,6 +9,9 @@ from django.contrib.auth.decorators import login_required
 
 from random import sample
 
+import sys
+
+import bin.crawl as crawl
 
 def signup(request):
     if request.method == 'POST':
@@ -37,5 +40,6 @@ def rand_songs(req, n):
     return JsonResponse([[s for s in songs], pairs], safe=False)
 
 def random_walk(req, id):
-    d = {id:id}
-    return JsonResponse(d)
+    spotify = crawl.Spotify('BQD3aEb1QpWDYzYFLio2snslfTgJ_WictCNpZE4ojRnBbgrWPjP1l6YYad6A8lRzJDeOi6XAEhUT8XHklUY')
+    return JsonResponse(spotify.track_recommendations(id))
+#    return JsonResponse(d)
