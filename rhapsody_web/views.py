1ef18d41f9313a790dcdf303336e90297b536891
Madeline Kusters <mkusters@nd.edu>
2018-05-01 23:30:50 -0400

    random walk for recommendations started

--- rhapsody_web/views.py
+++ rhapsody_web/views.py
@@ -9,9 +9,10 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 
 from json import loads
-from random import sample
+from random import sample, random, choice
 
 import sys
+import operator
 
 import bin.crawl as crawl
 
@@ -42,7 +43,7 @@ def rand_songs(req, n):
     return JsonResponse([[s for s in songs], pairs], safe=False)
 
 
-def random_walk(req, spotify_id):
+def nearest_neighbors(req, spotify_id):
     # spotify = crawl.Spotify('BQD3aEb1QpWDYzYFLio2snslfTgJ_WictCNpZE4ojRnBbgrWPjP1l6YYad6A8lRzJDeOi6XAEhUT8XHklUY')
     # return JsonResponse(spotify.track_recommendations(id))
     # return JsonResponse(d)
@@ -61,3 +62,49 @@ def random_walk(req, spotify_id):
     nodes = loads(serialize('json', list(g)))
     edges = [[a.spotify_id, b.spotify_id] for a, b in node.edges(DEPTH)]
     return JsonResponse([nodes, edges], safe=False)
+
+
+
+def random_walk_n_recommendations(spotify_id):
+    try:
+        start_node = models.Song.objects.get(pk=spotify_id)
+    except models.Song.DoesNotExist:
+        try:
+            start_node = models.Artist.objects.get(pk=spotify_id)
+        except models.Artist.DoesNotExist:
+            try:
+                start_node = models.Album.objects.get(pk=spotify_id)
+            except models.Album.DoesNotExist:
+                pass
+
+    prob_go_home = 0.5
+
+    current_node = start_node
+
+    visits = {}
+
+    #    random.seed()
+
+    for i in range(100):
+        if current_node not in visits:
+            visits[current_node] = 1
+        else:
+            visits[current_node] += 1
+
+        if random() <= prob_go_home:
+            current_node = start_node
+        else:
+            #randomly choose a node from the neighbors for the next node
+            current_node = choice(list(current_node.neighbors()))
+
+    #print(len(visits))
+
+    sorted_visits = sorted(visits.items(), key=operator.itemgetter(1))
+    print(start_node)
+    if start_node in sorted_visits:
+        print("removing start_node")
+        sorted_visits.remove(start_node)
+
+    print(len(sorted_visits))
+    print(sorted_visits[0])
+    return sorted_visits[0:5]
