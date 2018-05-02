d1b02c50ead76ffb28ebaf96dad5cf35534f070e
Will Badart <wbadart@live.com>
2018-05-02 00:09:36 -0400

    Fixed api url handler

--- rhapsody_web/urls.py
+++ rhapsody_web/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('signup/', views.signup, name='signup'),
     path('test/<int:n>', views.rand_songs, name='test'),
-    path('api/<str:spotify_id>', views.random_walk, name='walk'),
+    path('api/<str:spotify_id>', views.nearest_neighbors, name='walk'),
     path('recommend/<str:name>', views.recommend, name='recommend'),
 ]
